640f4cdd694f4adafd53236578e9531fefdd0317
1C0D <63018905+1C0D@users.noreply.github.com>
2020-07-29 14:16:28 +0200

    multifile selection added and multi install

--- Addon_Installer-Script_Runner.py
+++ Addon_Installer-Script_Runner.py
@@ -8,7 +8,8 @@ bl_info = {
     "name": "Addon Installer|Script Runner",
     "description": "install save reload addons or run scripts just selecting a file",
     "author": "1C0D and from Amaral Krichman's addon",
-    "version": (1, 0, 0),
+    # multi selection file added for multi addons installation
+    "version": (1, 1, 0),
     "blender": (2, 83, 0),
     "location": "Global/Text Editor",
     "warning": "",
@@ -16,70 +17,82 @@ bl_info = {
     "category": "Development",
 }
 
-#----------------------------- INSTALLER FROM FILES
+# ----------------------------- INSTALLER FROM FILES
+
 
 class INSTALLER_OT_FileBrowser(bpy.types.Operator, ImportHelper):
     bl_idname = "installer.file_broswer"
     bl_label = "install addon run ext script from files"
-    
+
     filter_glob: bpy.props.StringProperty(
         default='*.py;*.zip;*.txt',
         options={'HIDDEN'},
         subtype='FILE_PATH'  # to be sure to select a file
     )
 
-    def execute(self, context):        
-        
+    files: bpy.props.CollectionProperty(type=bpy.types.PropertyGroup)
+    # https://blender.stackexchange.com/questions/30678/bpy-file-browser-get-selected-file-names
+
+    def execute(self, context):
+
         print('#'*50)
         p = self.filepath
-        print('p: ', p)
+        dirname = os.path.dirname(self.filepath)
         name = Path(p).stem
-        
+
         # check if bl_info (Script or not)
-        Script=True
-        if Path(p).suffix == '.py' or Path(p).suffix == '.txt':      # == and not is!              
+        Script = True
+        if Path(p).suffix == '.py' or Path(p).suffix == '.txt': 	 # == and not is!
             with open(p, 'r') as f:
-                body = f.readlines() 
+                body = f.readlines()
                 for line in body:
                     if 'bl_info' in line and not line.startswith("#"):
                         print('addon')
-                        Script=False
-                        
+                        Script = False
+
         if Path(p).suffix == '.zip':
-            Script=False            
+            Script = False
 
         if Script:
             print('not addon')
-            exec(compile(open(p).read(), p, 'exec')) #run script
-            self.report({'INFO'}, "RUN SCRIPT: "+ name)
+            exec(compile(open(p).read(), p, 'exec'))  # run script
+            self.report({'INFO'}, "RUN SCRIPT: " + name)
             return {'FINISHED'}
-            
+
         else:
-            #disable
-            try:
-                bpy.ops.preferences.addon_disable(module=name)
+            for f in self.files:
+                p = os.path.join(dirname, f.name)
+                name = Path(p).stem
+
+                # disable
+                try:
+                    bpy.ops.preferences.addon_disable(module=name)
+                    time.sleep(0.05)
+                except RuntimeError:
+                    self.report({'ERROR'}, "ADD-ON ERROR, see in the console")
+                    return {'CANCELLED'}
+
+                # remove/install
+                bpy.ops.preferences.addon_remove(module=name)
                 time.sleep(0.05)
-            except RuntimeError:
-                self.report({'ERROR'}, "ADD-ON ERROR, see in the console")
-                return {'CANCELLED'}
-
-            # remove/install
-            bpy.ops.preferences.addon_remove(module=name)
-            time.sleep(0.05)
-            bpy.ops.preferences.addon_install(filepath=p)
-            time.sleep(0.05)
-
-            # enable
-            try:
-                bpy.ops.preferences.addon_enable(module=name)
+                bpy.ops.preferences.addon_install(filepath=p)
                 time.sleep(0.05)
-            except RuntimeError:
-                self.report({'ERROR'}, "ADD-ON ERROR, see in the console")
-                return {'CANCELLED'}
 
-            self.report({'INFO'}, "INSTALLED/RELOADED: " + name)  
+                # enable
+                try:
+                    bpy.ops.preferences.addon_enable(module=name)
+                    time.sleep(0.05)
+                except RuntimeError:
+                    self.report({'ERROR'}, "ADD-ON ERROR, see in the console")
+                    return {'CANCELLED'}
+
+                if len(self.files) < 2:
+                    self.report({'INFO'}, "INSTALLED/RELOADED: " + name)
+            if len(self.files) > 1:
+                self.report({'INFO'}, "MULTI INSTALLED/RELOADED ")
+
             return {'FINISHED'}
-        
+
 
 def draw(self, context):
 
@@ -88,7 +101,8 @@ def draw(self, context):
     layout.operator("installer.file_broswer",
                     text="Install-Reload Addon | Run ext Script", icon='FILEBROWSER')
 
-#----------------------------- INSTALLER FROM TEXT EDITOR
+# ----------------------------- INSTALLER FROM TEXT EDITOR
+
 
 class INSTALLER_OT_TextEditor(bpy.types.Operator):
 
@@ -144,7 +158,7 @@ class INSTALLER_OT_TextEditor(bpy.types.Operator):
             self.report({'ERROR'}, "ADD-ON ERROR, see in the console")
             return {'CANCELLED'}
 
-        self.report({'INFO'}, "Installed/Reloaded: " +name)
+        self.report({'INFO'}, "Installed/Reloaded: " + name)
 
         return {'FINISHED'}
 
@@ -152,33 +166,32 @@ class INSTALLER_OT_TextEditor(bpy.types.Operator):
 def text_addon_refresh(self, context):
 
     self .layout.separator(factor=1.0)
-    self.layout.operator("installer.text_editor",text="Install-Reload Addon from Text Editor", icon='COLLAPSEMENU')
+    self.layout.operator("installer.text_editor",
+                         text="Install-Reload Addon from Text Editor", icon='COLLAPSEMENU')
 
     return {'FINISHED'}
-
 
 
 classes = (INSTALLER_OT_FileBrowser, INSTALLER_OT_TextEditor)
 
 addon_keymaps = []
 
+
 def register():
-    
-    
-    #classes
+
+    # classes
     for c in classes:
         bpy.utils.register_class(c)
-        
-    #menus entries    
+
+    # menus entries
     bpy.types.TEXT_MT_text.append(draw)
     bpy.types.TOPBAR_MT_app.append(draw)
     bpy.types.TEXT_MT_text.append(text_addon_refresh)
-        
 
-    #key
+    # key
     wm = bpy.context.window_manager
     kc = wm.keyconfigs.user
-   
+
     for k in kc.keymaps["Text"].keymap_items:
         if k.idname == "wm.call_menu" and k.properties.name == "SCREEN_MT_user_menu" and k.active:
             if k.properties.name == "SCREEN_MT_user_menu":
@@ -186,14 +199,14 @@ def register():
 
     kc = wm.keyconfigs.addon
     km = kc.keymaps.new(name='Text', space_type='TEXT_EDITOR')
-    kmi = km.keymap_items.new("wm.call_menu", "Q", "PRESS",ctrl=True)
+    kmi = km.keymap_items.new("wm.call_menu", "Q", "PRESS", ctrl=True)
     kmi.properties.name = "SCREEN_MT_user_menu"
     addon_keymaps.append((km, kmi))
 
 
 def unregister():
-    
-    #key
+
+    # key
     wm = bpy.context.window_manager
     kc = wm.keyconfigs.user
     for k in kc.keymaps["Text"].keymap_items:
@@ -204,14 +217,14 @@ def unregister():
                         km.keymap_items.remove(kmi)
                 except:
                     pass
-            
-    addon_keymaps.clear() 
 
-    #menus entries
+    addon_keymaps.clear()
+
+    # menus entries
     bpy.types.TEXT_MT_text.remove(draw)
     bpy.types.TOPBAR_MT_app.remove(draw)
     bpy.types.TEXT_MT_text.remove(text_addon_refresh)
 
-    #classes
+    # classes
     for c in classes:
         bpy.utils.unregister_class(c)
